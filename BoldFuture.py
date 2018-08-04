aee3aed353675f4e6ceb82a709e70d69407a27a8
Christian Merrill <christian.j.merrill@gmail.com>
2018-08-03 21:04:55 -0400

    Revert "added loop functionality"
    This reverts commit 580278488f7983485c1b1f2f6287a9eb7c352847.

--- BoldFuture.py
+++ BoldFuture.py
@@ -384,20 +384,16 @@ class Botty(sc2.BotAI):
                 choice = random.randrange(0,17)
             try:
                 await self.choices[choice]()
-            except:
-                pass
+            except Exception as e:
+                print(str(e))
+                print(self.choices[choice])
             y = np.zeros(17)
             y[choice] = 1
             self.trainingData.append([y,self.flipped])
 
 
 
-def main():
-    run_game(maps.get("Abyssal Reef LE"), [
-        Bot(Race.Protoss, Botty(use_model=False, title=1)), Computer(Race.Terran, Difficulty.Easy)
-    ], realtime=False)
 
-
-if __name__ == "__main__":
-    for x in range(0,10):
-        main()
+run_game(maps.get("Abyssal Reef LE"), [
+    Bot(Race.Protoss, Botty(use_model=False, title=1)), Computer(Race.Terran, Difficulty.Easy)
+], realtime=False)
